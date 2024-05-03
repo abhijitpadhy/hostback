b0bb28723f9ab56724a88b32cdac54c30f99aea0
unknown <abhijitpadhy2000@gmail.com>
2024-05-03 18:54:37 +0530

    last

---
+++ keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoUrl: "mongodb+srv://amishananda2000:awSB07Z0aGPKNHeS@cluster0.5ztxodd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    Jwt_secret: "sfuttgukhkjmh" 
+};
